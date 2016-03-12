4082e8b08fbb2e27ed13832a3304702da547e5b0
violetaria <terri.chu@outlook.com>
2016-03-11 22:18:54 -0500

    controller, views, and routes

--- app/controllers/trusted_applications_controller.rb
+++ app/controllers/trusted_applications_controller.rb
@@ -1,1 +1,23 @@
-class AppsController
+class TrustedApplicationsController < ApplicationController
+
+  def index
+    @trusted_apps = TrustedApplication.all
+    render 'index.json.jbuilder', status: :ok
+  end
+
+  def create
+    @trusted_app = TrustedApplication.new(trusted_app_params)
+    @trusted_app.save
+    render 'create.json.jbuilder', status: :created
+  end
+
+  def show
+    @trusted_app = TrustedApplication.find(params[:id])
+    render 'show.json.jbuilder', status: :ok
+  end
+
+  private
+  def trusted_app_params
+    params.require(:application).permit(:name)
+  end
+end
